947ae1e5779db317fc3f4ceb3fa1a15dc3883276
haile17 <hailefikere17@gmail.com>
2024-11-14 12:55:42 +0300

    update

--- bot.py
+++ bot.py
@@ -1,22 +1,20 @@
-from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import Application, CommandHandler, CallbackContext
 
-# Replace 'YOUR_BOT_TOKEN' with the token you received from BotFather
-BOT_TOKEN = 'Y7699996331:AAHK8AWzaOqgd1K2XoyFDYukAYI7RUCTyo8'
-updater = Updater(BOT_TOKEN)
+# Create an Application object
+application = Application.builder().token("7699996331:AAHK8AWzaOqgd1K2XoyFDYukAYI7RUCTyo8").build()
 
 # Define the start command function
-def start(update: Update, context: CallbackContext) -> None:
+async def start(update: Update, context: CallbackContext) -> None:
     # Define the button that opens your Web App link
     keyboard = [
         [InlineKeyboardButton("View Airdrops", web_app=WebAppInfo(url="https://airdropworld.netlify.app/"))]
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
-    update.message.reply_text("Click below to view the latest airdrops!", reply_markup=reply_markup)
+    await update.message.reply_text("Click below to view the latest airdrops!", reply_markup=reply_markup)
 
-# Add the start command handler
-updater.dispatcher.add_handler(CommandHandler("start", start))
+# Add command handler
+application.add_handler(CommandHandler("start", start))
 
-# Start the bot
-updater.start_polling()
-updater.idle()
+# Run the bot
+application.run_polling()
